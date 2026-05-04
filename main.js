426c5a3cebd6e183b25943675659b3ab06569788
GuilhermeVitali <guilherme.albrecht@escola.pr.gov.br>
2026-05-04 08:02:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-ocnteudo");
+
 
 for(let i = 0; i < botoes.length; i++){
     botoes [i].onclick = () => {
         for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.add("ativo");
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+
     }
 }
